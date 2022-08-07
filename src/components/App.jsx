4d2a74c53bf57e69767b26b8260a0700727d1537
jay-rt <thapa.jayrt@gmail.com>
2022-08-07 18:46:12 -0400

    replaced hard coded value with dynamic values using array and maps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import List from "./List";
+import notes from "../notes";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <List />
+      {notes.map((note) => (
+        <List key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
